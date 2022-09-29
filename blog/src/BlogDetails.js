d6383f47d622c20b2d29840c11d3bc2e3442750c
kevinasian122 <yanchenkai@gmail.com>
2022-09-29 14:26:16 -0400

    added feature to add blogs

--- blog/src/BlogDetails.js
+++ blog/src/BlogDetails.js
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const {id} = useParams()
+    const {id} = useParams() //Gets the ID from app.js
+    const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/'+id);
+    {/*uses useFetch hook again, just pass in endpoint and it returns all this data, 
+        btw data:blog is just calling data "blog" here*/}
     return ( 
         <div className="blog-details">
-            <h2>
-                Blog Details - {id}
-            </h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>Error</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+
+                </article>
+            )}
         </div>
      );
 }
--- blog/src/Create.js
+++ blog/src/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 const Create = () => {
+    const[title, setTitle] = useState('');  
+    const[body, setBody] = useState('');  
+    const[author, setAuthor] = useState('mario');
+    const[isPending, setIsPending] = useState(false);
+    const history = useHistory(); //accesses history like the back and forwar arrows
+
+    const handleSubmit = (e) =>{ //preventDefault makes it not refresh, and then blog is an object created
+        e.preventDefault()
+        const blog = {title, body, author};
+        setIsPending(true)
+        fetch('http://localhost:8000/blogs', { //posts the new blog into json
+            method:'POST',
+            headers: {"Content-Type": "applicatoin/json"}, 
+            body: JSON.stringify(blog)
+        }).then(()=> {
+            setIsPending(false);
+            history.push('/');
+        })
+    }
+
     return ( 
         <div className="create">
-            <h2>add a new blog</h2>
+            <h2>add a new blog</h2> 
+            <form onSubmit={handleSubmit}> {/* form has onsubmit function whenever a button is clicked inside */}
+                <label>Blog title:</label>
+                <input 
+                    type = "text"
+                    required
+                    value = {title}
+                    onChange = {(e) => setTitle(e.target.value)}
+                    />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value = {body}
+                    onChange = {(e) => setBody(e.target.value)}
+                    />
+                <label>Blog author:</label>
+                <select
+                    value = {author}
+                    onChange = {(e) => setAuthor(e.target.value)}
+                >
+                    <option value = "mario">mario</option>
+                    <option value = "yoshi">yoshi</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabeled>Adding blog ...</button>}
+            </form>
         </div>
      );
 }
